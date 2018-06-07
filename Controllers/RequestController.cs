3d23b10667359720885ac00e76e81f96a4481742
Nick Duijvelshoff <nick@duijvelshoff.com>
2018-06-07 23:28:41 +0200

    Added RequestController - Must be used via: application/x-www-form-urlencoded Values are: amount="0.01" recipient="online@duijvelshoff.com" description="Give me my money!"

---
+++ Controllers/RequestController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using BunqAggregation;
+using Bunq.Sdk.Context;
+using Bunq.Sdk.Model.Generated.Endpoint;
+using Bunq.Sdk.Model.Generated.Object;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BunqAggregation.Controllers
+{
+    [Route("api/[controller]")]
+    public class RequestController : Controller
+    {
+        [HttpPost]
+        public void Post()
+        {
+            JObject config = Settings.LoadConfig();
+
+            Console.WriteLine("Hi there, we are connecting to the bunq API...\n");
+
+            var apiContext = ApiContext.Restore();
+            BunqContext.LoadApiContext(apiContext);
+            Console.WriteLine(" -- Connected as: " + BunqContext.UserContext.UserPerson.DisplayName + " (" + BunqContext.UserContext.UserId + ")\n");
+
+
+            var amount = Request.Form["amount"].ToString();
+            string recipient = Request.Form["recipient"].ToString();
+            string description = Request.Form["description"].ToString();
+
+            Console.WriteLine("Todo:");
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Description:           " + description);
+            Console.WriteLine("Send to:               " + recipient);
+            Console.WriteLine("Amount:                € " + amount);
+            Console.WriteLine("----------------------------------------------------------");
+
+            Console.WriteLine("Executing...");
+            RequestInquiry.Create(new Amount(amount, "EUR"), new Pointer("EMAIL", recipient), description, true);
+            Console.WriteLine("Yeah, this one is completed!");
+            Console.WriteLine("----------------------------------------------------------\n");
+        }
+    }
+}
